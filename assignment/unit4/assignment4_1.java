dc5f12237851c91b6ac6a95e5db2a55ea30ed5d8
fuzihaolizihao <fzhfzh102030@sina.com>
2025-01-03 10:48:27 +0800

    unit4 assinment

--- assignment/unit4/assignment4_1.java
+++ assignment/unit4/assignment4_1.java
@@ -1,11 +1,10 @@
-package assignment.unit4;
 
 import java.io.IOException;
 import java.util.logging.FileHandler;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 
-public class assignment4_1 {
+class assignment4_1 {
     private static final Logger logger = Logger.getLogger(assignment4_1.class.getName());
 
     public static void main(String[] args) {
